461c55d18082e7f736f7f218cd45f93267e49ecd
Roee Landesman <roee.landesman@gmail.com>
2018-08-06 14:06:59 -0700

    iterate through folder

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #TODO: Take the file name as an input (DONE)
-#TODO: Take a folder as an input (loop through and print the comment number of each)
-#TODO: Language support beyond python #
+#TODO: Take a folder as an input (loop through and print the comment number of each) (DONE)
+#TODO: Make data printing prettier basically
+#TODO: Language support beyond python #, depending on file type of file being read in.
 #TODO: Integrate database?
 
 import io
 import textwrap
 import argparse
 import os
+import tabulate
 
-file_name = 'sample.txt'
+def get_arguments():
+    """ The argument parser of the command-line version """
+    parser = argparse.ArgumentParser(description=('Comment counting program'))
+
+    parser.add_argument('--input_folder', '-i',
+                        help="name of the input folder", type=lambda x: is_valid_file(parser, x),
+                        dest='i')
+
+    args = parser.parse_args()
+    return args
 
 def numberOfComments(file_name):
     isComment = False
@@ -45,20 +56,26 @@ def print_data(file_name, number_of_comments,number_of_lines):
     In the file: %s, there are %s comment lines
     """ %(number_of_lines, file_name, number_of_comments))
 
-def get_arguments():
-    """ The argument parser of the command-line version """
-    parser = argparse.ArgumentParser(description=('Comment counting program'))
-
-    parser.add_argument('--input_folder', '-i',
-                        help="name of the input folder", type=lambda x: is_valid_file(parser, x),
-                        dest='i')
-
-    args = parser.parse_args()
-    return args
+def iterate_folder(folder_name):
+    """
+    Put the names of each file in the directory into an iteratable list
+    """
+    returnList = []
+    for file in os.listdir(folder_name):
+        if file.endswith(".txt") or file.endswith(".py"):
+            returnList.append(file)
+    return returnList
 
 if __name__ == "__main__":
+    returnTable = []
     args = get_arguments()
     input_folder_name = args.i
-    comment_count = numberOfComments(input_folder_name)
-    line_count = numberOfTotalLines(input_folder_name)
-    print_data(input_folder_name, comment_count, line_count)
+    list_files = iterate_folder(input_folder_name)
+    for oneFile in list_files:
+        comment_count = numberOfComments(oneFile)
+        line_count = numberOfTotalLines(oneFile)
+        false_tuple = []
+        false_tuple.append(line_count)
+        false_tuple.append(comment_count)
+        returnTable.append(false_tuple)
+    print(returnTable)
