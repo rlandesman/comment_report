3b45bca6489ec7c6af6d808e3996c0028e32f741
Roee Landesman <roee.landesman@gmail.com>
2018-08-06 11:33:53 -0700

    add groundwork functions

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
+#TODO: Take the file name as an input
+#TODO: Take a folder as an input (loop through and print the comment number of each)
+#TODO: Language support beyond python #
+
 import io
+import textwrap
 
-isComment = False
-x = 0
+file_name = 'sample.txt'
 
-with open('python_sample.py') as input:
-    for line in input:
-        isComment == False
-        for char in line:
-            if char == "#":
-                isComment = True
-            else:
-                pass
-        if (isComment == True):
-            x = x + 1
-print(x)
+def numberOfComments(file_name):
+    isComment = False
+    x = 0
+    with open(file_name) as input:
+        for line in input:
+            isComment = False
+            for char in line:
+                if char == "#":
+                    isComment = True
+                else:
+                    pass
+            if (isComment == True):
+                x = x + 1
+        return(x)
+
+def prettyPrint(file_name, number_of_comments):
+    print textwrap.dedent("""\
+    In the file: %s, there are %s comment lines
+    testing test
+    """ %(file_name,number_of_comments))
+
+def main():
+    comment_count = numberOfComments(file_name)
+    prettyPrint(file_name, comment_count)
+
+main()
