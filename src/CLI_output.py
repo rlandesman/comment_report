1ebe86fe4cf8eb3ad463fcc10a3b47420524ef72
Roee Landesman <roee.landesman@gmail.com>
2018-08-13 15:22:22 -0700

    refactor complete and tested

--- src/CLI_output.py
+++ src/CLI_output.py
@@ -1,79 +1,9 @@
 from tabulate import tabulate
 from main import *
-import os
-
-new_repo_name = "new_repo" #Make this variable with name of repo being cloned -> no cleanup needed
-
-def filter_file(single_file):
-    """ Method used to filter out unwanted files """
-    if(single_file.endswith(".txt")
-        or single_file.endswith(".py")
-        or single_file.endswith(".java")
-        or single_file.endswith(".s")):
-            return single_file
-
-def match_comment_type(file_name):
-    """
-    Check the type of a single file and return the correct charachter that needs to be checked for comments
-    # -> python
-    // -> Java
-    # -> Textfile (my preference I recognize this loophole)
-    """
-    if(file_name.endswith(".txt")):
-        return "#"
-    elif(file_name.endswith(".py")):
-        return "#"
-    elif(file_name.endswith(".java")):
-        return "/"
-    elif(file_name.endswith(".s")):
-        return "@"
-    else:
-        return "null"
 
 def define_headers():
     headers=["File Name","Line Count","Comment Count","Ratio"]
     return headers
-
-def get_full_path(root_folder):
-    """ Takes the name of a root folder and returns list of all the full name paths to each file"""
-    absolute_list = []
-    for dirpath,_,filenames in os.walk(root_folder):
-        for f in filenames:
-            absolute_list.append((os.path.abspath(os.path.join(dirpath, f))))
-    return absolute_list
-
-def parse_flag_type(args):
-    """ Input --> namespace object and output --> raw directory location of flag object"""
-    if (args.input_folder != None):
-        return args.input_folder
-    elif (args.input_repo != None): # Return full path directory name
-        os.system("git clone " + args.input_repo + " " + new_repo_name)
-        return os.path.abspath(new_repo_name)
-    else:
-        pass
-
-def gather_data(args):
-    """ Output all data into a tabulated form and out to CLI """
-    returnTable = []
-    directory = parse_flag_type(args)
-    os.chdir(directory)
-    absolute_list = get_full_path(directory)
-    for oneFile in absolute_list:
-        if filter_file(oneFile):
-            comment_count = compute_comment_stats(oneFile, match_comment_type(oneFile))
-            line_count = compute_linenumber_stats(oneFile)
-            ratio = round(float(comment_count)/float(line_count),3) #To the third decimal spot
-            stats = []
-            stats.append(oneFile)
-            stats.append(line_count)
-            stats.append(comment_count)
-            stats.append(ratio)
-            returnTable.append(stats)
-        else:
-            pass
-
-    return returnTable
-
 
 def print_to_CLI(table, args):
     headers = define_headers()
--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ import os
 from tabulate import tabulate
 from CLI_output import *
 from visualize import *
+from stats import *
 
 def get_arguments():
     """ The argument parser of the command-line version """
@@ -26,53 +27,6 @@ def get_arguments():
     args = parser.parse_args()
     return args
 
-def compute_comment_stats(file_name, comment_char):
-    """ Collect data on number of comments in the current file """
-    isComment = False
-    x = 0
-
-    with open(file_name) as input:
-        idx = 0
-        for line in input:
-            isComment = False
-            while idx < (len(line) - 1):
-                if(comment_char == "#"):
-                    if (line[idx] == comment_char and line[idx+1] != '"'  and line[idx-1] != '"'):
-                        isComment = True
-                    else:
-                        pass
-                if(comment_char=="/"):
-                    if (line[idx+1]==comment_char and line[idx]==comment_char):
-                        isComment = True
-                    else:
-                        pass
-                if(comment_char=="@"):
-                    if (line[idx]==comment_char):
-                        isComment = True
-                    else:
-                        pass
-                idx+=1
-            if (isComment == True):
-                x = x + 1
-            idx=0
-        return(x)
-
-def compute_linenumber_stats(file_name):
-    """ Collect data on number of total lines in the current file """
-    x = 0
-    with open(file_name) as input:
-        for line in input:
-            x = x + 1
-    return(x)
-
-def is_valid_file(parser, arg):
-    """ Check if file can be opened (legit path?) """
-    arg = os.path.abspath(arg)
-    if not os.path.exists(arg):
-        parser.error("The file %s does not exist!" % arg)
-    else:
-        return arg
-
 def delete_single_dir(args):
     if (args.input_folder != None):
         pass
@@ -82,6 +36,7 @@ def delete_single_dir(args):
         os.system("rm -rf " + current)
     else:
         pass
+
 
 if __name__ == "__main__":
     args = get_arguments()
